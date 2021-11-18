264484ed6c47ff656c9cabedbf8ad9f00f0ac312
mickelv <47368105+mickelv@users.noreply.github.com>
2021-11-18 14:11:35 -0800

    product data laptop work

---
+++ utils/data.js
@@ -0,0 +1,13 @@
+const data = {
+    products: [
+        {
+            name:'Piña Explosion',
+            category:'Fruity',
+            price: 15,
+            rating:5,
+            numReviews:2,
+            numInStock:5,
+            description:'A fruity piña explosion in your mouth',
+        },
+    ],
+}
